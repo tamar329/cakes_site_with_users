02b69dbcd574a60e2844d24e1cb216db20800298
tamar329 <tamar7475@gmail.com>
2023-06-01 12:34:33 +0300

    change index message

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express= require("express");
 const router = express.Router();
 
 router.get("/" , (req,res)=> {
-  res.json({msg:"Rest api work !"})
+  res.json({msg:"Rest api work!"})
 })
 
 module.exports = router;
