131b226d1cda3593c4bb613c60dcaf69985f3e4a
tamar329 <tamar7475@gmail.com>
2023-06-01 12:15:50 +0300

    Fix secret incident

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -5,8 +5,7 @@ const config = require("../config/secret")
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb+srv://tamar:tamar329@cluster0.nmb9jb3.mongodb.net/black23');
-  // await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.nmb9jb3.mongodb.net/black23`);
+  await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.nmb9jb3.mongodb.net/black23`);
   console.log("mongo connect users");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
